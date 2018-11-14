f2748f82ed8c209bfc7106cfb06e72c9f5d220e7
Dougey-G <171282@student.asd20.org>
2018-11-14 15:43:28 -0700

    File IO: finished Character: Added comments for each characteristic, need to do syntax now.

---
+++ Archive 11-2-18/Programming/Character/Character/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Character
+        {
+            string CharacterName;
+            // Name
+            //{
+            // Console.ReadLine(string.Parse) or string.Parse(Console.ReadLine);
+            // Console.WriteLine("Please enter your name)
+            //}
+
+            string Height;
+            // Height
+            //{
+            // int.Parse(Console.ReadLine());
+            // result = '0';
+            // Console.WriteLine("Please enter your height in inches(Hint: 12 inches = 1ft)":
+            // 
+            //}
+
+            string EyeColor;
+            // Red, Blue, Green, Yellow, Purple, Orange, Light Green, Light Blue, Hazel
+            // Eye Color
+
+            string SkinColor;
+            // string.Parse(Console.ReadLine)
+            // White, Tan, Brown/Black (African American) 
+            // Skin Color
+
+            string HairColor;
+            // Blonde, Brunette, Black, Red Head/Ginger, 
+            // Hair Color
+
+        }
+    }
+}
---
+++ Archive 11-2-18/Programming/File IO/File IO/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"C:\Users\171282\Documents\Senior-Year-Coding-Class\Archive 11-2-18\Example.txt";
+
+            //StreamWriter sw = new StreamWriter(path);
+            //sw.WriteLine("Hello,");
+            //sw.WriteLine("It's a me, Mario.");
+            //sw.Dispose();
+
+            //string path = @"C:\Users\171282\Documents\Senior-Year-Coding-Class\Archive 11-2-18\Example.txt";
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Welcome");
+            //    sw.WriteLine("New Galaxy");
+            //}
+
+            //string path;
+            //path = @"C:\Users\171282\Documents\Senior-Year-Coding-Class\Archive 11-2-18\Example.txt";
+            //path = AppDomain.CurrentDomain.BaseDirectory + @"Example.txt";
+
+            string path = @"C:\Users\171282\Documents\Senior-Year-Coding-Class\Archive 11-2-18\Example.txt";
+
+            List<string> file = new List<string>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    file.Add(line);
+                }
+            }
+
+            string result = Console.ReadLine();
+
+            if (result == "1")
+            {
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine("Hello,");
+                sw.WriteLine("It's a me, Mario.");
+                sw.Dispose();
+            }
+
+            if (result == "2")
+            {
+                StreamReader sr = new StreamReader(path);
+                //sr.ReadLine("Hello");
+                //sr.
+            }
+        }
+    }
+}
