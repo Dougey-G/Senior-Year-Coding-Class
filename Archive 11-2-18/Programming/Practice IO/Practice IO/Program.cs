9a867189c71f2a3d55971f905db45bb8402dc78f
Dougey-G <171282@student.asd20.org>
2018-11-16 15:54:34 -0700

    Added some code for Create, and Load File

--- Archive 11-2-18/Programming/Practice IO/Practice IO/Program.cs
+++ Archive 11-2-18/Programming/Practice IO/Practice IO/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Practice_IO
 {
@@ -11,19 +12,35 @@ namespace Practice_IO
         static void Main(string[] args)
         {
             string result = Console.ReadLine();
+            string path = @"C:\Users\171282\Documents\Senior-Year-Coding-Class\Archive 11-2-18\Mario.txt";
+            List<string> file = new List<string>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    file.Add(line);
+                }
+            }
             Console.WriteLine("1. Create File");
             Console.WriteLine("2. Delete File");
             Console.WriteLine("3. Load File");
             // Look at 2 links
 
-            if (result == '1')
+            if (result == "1")
             {
-
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine("(insert character attributes");
             }
 
-            if (result == '2')
+            if (result == "2")
             {
+                
+            }
 
+            if (result == "3")
+            {
+                StreamReader sr = new StreamReader(path);
             }
         }
     }
