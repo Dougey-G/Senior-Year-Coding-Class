508ece0a09d7400cdc29e515a2203952a8cd5750
Dougey-G <171282@student.asd20.org>
2019-01-09 16:24:50 -0700

    Finished Stack project (Double check to see if there's anything else I need to do)

---
+++ Semester 2/Stack/Stack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+                
+            Stack myStack = new Stack();
+            do
+            { 
+            Console.WriteLine("1. Push");
+            Console.WriteLine("2. Pop");
+            Console.WriteLine("3. Peek");
+            Console.WriteLine("4. Print");
+            Console.WriteLine("5. Exit");
+
+            result = Console.ReadLine();
+            
+                if (result == "1")
+                {
+                    Console.WriteLine("Please enter number you would like to add.");
+                    int Add = int.Parse(Console.ReadLine());
+                    myStack.Push(Add);
+                    Console.WriteLine();
+                }
+
+                if (result == "2")
+                {
+                    int Popval = myStack.Pop();
+                    Console.WriteLine("The top number in the stack is a " + Popval);
+                    Console.WriteLine();
+                }
+
+                if (result == "3")
+                {
+                    Console.WriteLine("The top value of the stack is " + myStack.Peek());
+                    Console.WriteLine();
+                }
+
+                if (result == "4")
+                {
+                    myStack.Print();
+                    Console.WriteLine();
+                }
+            } while (result != "5");
+            
+        }
+    }
+}
---
+++ Semester 2/Stack/Stack/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        List<int> holder = new List<int>();
+
+        public Stack()
+        {
+           
+        }
+
+        //Push
+        public void Push(int val)
+        {
+            holder.Add(val);
+        }
+
+        //Pop
+        public int Pop()
+        {
+            // holds the number, then deletes the "top" or first number from the stack
+            int Top = holder[holder.Count - 1];
+            holder.RemoveAt(holder.Count - 1);
+
+            return Top;
+        }
+
+        //Peek
+        public int Peek()
+        {
+            int Top = holder[holder.Count - 1];
+            return Top;
+        }
+    
+        //Print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count; i++) 
+            {
+                Console.WriteLine("The stack currently has " + holder[i]);
+            }
+        }
+    }
+}
