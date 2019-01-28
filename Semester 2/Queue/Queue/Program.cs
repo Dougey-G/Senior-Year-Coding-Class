d50fa0b509bed09a2dfd0bb38802d79ea027c109
Dougey-G <171282@student.asd20.org>
2019-01-28 15:57:52 -0700

    Finished Queue Project.

--- Semester 2/Queue/Queue/Program.cs
+++ Semester 2/Queue/Queue/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stack
+namespace Queue
 {
     class Program
     {
@@ -12,11 +12,11 @@ namespace Stack
         {
             string result;
 
-            Stack myStack = new Stack();
+            Queue myQueue = new Queue();
             do
             {
-                Console.WriteLine("1. Push");
-                Console.WriteLine("2. Pop");
+                Console.WriteLine("1. EnQueue");
+                Console.WriteLine("2. DeQueue");
                 Console.WriteLine("3. Peek");
                 Console.WriteLine("4. Print");
                 Console.WriteLine("5. Exit");
@@ -27,30 +27,29 @@ namespace Stack
                 {
                     Console.WriteLine("Please enter number you would like to add.");
                     int Add = int.Parse(Console.ReadLine());
-                    myStack.Push(Add);
+                    myQueue.EnQueue(Add);
                     Console.WriteLine();
                 }
 
                 if (result == "2")
                 {
-                    int Popval = myStack.Pop();
-                    Console.WriteLine("The top number in the stack is a " + Popval);
+                    int Popval = myQueue.DeQueue();
+                    Console.WriteLine("The top number in the queue is a " + Popval);
                     Console.WriteLine();
                 }
 
                 if (result == "3")
                 {
-                    Console.WriteLine("The top value of the stack is " + myStack.Peek());
+                    Console.WriteLine("The top value of the queue is " + myQueue.Peek());
                     Console.WriteLine();
                 }
 
                 if (result == "4")
                 {
-                    myStack.Print();
+                    myQueue.Print();
                     Console.WriteLine();
                 }
             } while (result != "5");
-
         }
     }
 }
---
+++ Semester 2/Queue/Queue/Queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue
+    {
+        
+        List<int> holder = new List<int>();
+
+        public Queue()
+        {
+
+        }
+
+        //EnQueue
+        public void EnQueue(int val)
+        {
+            holder.Add(val);
+        }
+
+        //DeQueue
+        public int DeQueue()
+        {
+            // holds the number, then deletes the "top" or first number from the queue
+            int Top = holder[0];
+            holder.RemoveAt(0);
+
+            return Top;
+        }
+
+        //Peek
+        public int Peek()
+        {
+            int Top = holder[0];
+            return Top;
+        }
+
+        //Print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count; i++)
+            {
+                Console.WriteLine("The queue currently has " + holder[i]);
+            }
+        }
+    }
+}
