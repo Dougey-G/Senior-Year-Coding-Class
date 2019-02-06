d11b0c35b243d01b307830e49453b93dfa064fb8
Dougey-G <171282@student.asd20.org>
2019-02-05 20:35:26 -0700

    no message

---
+++ Semester 2/Abstraction and Inheritance/Abstraction and Inheritance/Donkey Kong.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_and_Inheritance
+{
+    class Donkey_Kong : Animal
+    {
+        public Donkey_Kong(float weight, float height)
+           : base(weight, height)
+        {
+            isCarnivorous = false;
+        }
+
+        public override void Eat()
+        {
+            base.Eat();
+            Console.WriteLine(" Bananas!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("OO OO Aw!");
+        }
+    }
+}
