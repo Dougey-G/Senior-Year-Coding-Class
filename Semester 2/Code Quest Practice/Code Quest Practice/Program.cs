de80b17c5399312d933566e7603c9780fa91990c
Dougey-G <171282@student.asd20.org>
2019-02-09 13:55:24 -0700

    I've commented where I would appericate you checking Prob 3, and 4 to see if i'm on the right track.

--- Semester 2/Code Quest Practice/Code Quest Practice/Program.cs
+++ Semester 2/Code Quest Practice/Code Quest Practice/Program.cs
@@ -24,7 +24,7 @@ namespace Code_Quest_Practice
             }
 
             float total = 0;
-            
+
             for (int i = 0; i < file.Count; i++)
             {
                 if (file[i].Contains("QUARTER"))
@@ -67,16 +67,101 @@ namespace Code_Quest_Practice
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    file.Add(line);
+                    file2.Add(line);
                     //Console.WriteLine();
                 }
 
-                //if()
+                //if(int or string == random)
                 //{
-
+                Console.WriteLine("The numbers are in random order");
                 //}
 
+                //if(int or string == file2.Count + 1 )
+                //{
+                Console.WriteLine("The numbers are in ascending order");
+                //}
 
+                //if(int or string == file2.Count - 1 )
+                //{
+                Console.WriteLine("The numbers are in descending order");
+                //}
+
+                //if(int or string != int)
+                //{
+                Console.WriteLine("The input was invalid");
+                //}
+            }
+
+            string path3 = AppDomain.CurrentDomain.BaseDirectory + "Prob03.in.txt";
+            List<string> file3 = new List<string>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    file3.Add(line);
+                    //Console.WriteLine();
+                }
+
+                // Could you please check this part?
+
+                for (int i = 0; i < 26; i++)
+                {
+                    // int a = 0;
+                    //Decoder("a" == 0);
+                    //...
+                    // int m = 13;
+                    //Decoder("m" == 13);
+                    //...
+                    // int z = 26;
+                    //Decoder("z" == 26);
+                }
+
+                // Could you also please check this part?
+
+                //public static string IntToLetters(int value)
+                //{
+                //    string decoder = string.Empty;
+                //    while (value >= 0)
+                //    {
+                //        decoder = (char)('a' + value % 26) + result;
+                //        value-- == 26;
+                //    }
+                //    return result;
+                //}
+            }
+
+            string path4 = AppDomain.CurrentDomain.BaseDirectory + "Prob03.in.txt";
+            List<string> file4 = new List<string>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    file4.Add(line);
+                    //Console.WriteLine();
+                }
+
+                // Could you also please check this part too?
+
+                //factorials = n!;
+
+                //for (int i = 0; i < factorials.Length; i++)
+                //{
+                // i <= n!
+                // n! = n * n<=n
+                // factorials = number chosen * number chosen <= number chosen
+                //}
+
+                // You can ignore examples, just a helpful tip for me
+                //Examples
+                //1! = 1*1 = 1
+                //...
+                //4! = 4*3*2*1 = 24
+                //...
+                //25! = 25*24*23*22*21*20*19*18*17*16*15*14*13*12*11*10*9*8*7*6*5*4*3*2*1 = 1.5511e + 25
+                //...
+                //50! = 50*49*48*47*46*45*44*43*42*41*40*39*38*37*36*35*34*33*32*31*30*29*28*27*26*25*24*23*22*21*20*19*18*17*16*15*14*13*12*11*10*9*8*7*6*5*4*3*2*1 = 3.0414e + 64
             }
         }
     }
