d3fdbb1eb3e0cd861a96972745dfdebda7a1c36d
Dougey-G <171282@student.asd20.org>
2019-02-13 15:10:11 -0700

    Introduced to LINQ and Lambda Expressions.

---
+++ Semester 2/Intro to LINQ and Lambda Expressions/Intro to LINQ and Lambda Expressions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_to_LINQ_and_Lambda_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> L1 = new List<int>() { 8, 6, 7, 5, 3, 0, 9 };
+            //List<int> L2 = new List<int>() { 3, 2, 2, 8, 0, 4, 7 };
+            //List<int> L3 = new List<int>();
+
+            //l3 = l2.Intersect(L1).ToList();
+            //foreach(int i in L3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            List<int> L1 = new List<int>() { 8, 6, 7, 5, 3, 0, 9 };
+            List<int> L2 = new List<int>();
+
+            L2 = L1.Where(>= t - 2 == 0).ToList();
+
+            foreach(int i in L2)
+            {
+                Console.WriteLine(i + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
