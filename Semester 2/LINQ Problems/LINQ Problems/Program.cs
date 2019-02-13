bdff9c0c48ffa81fe03d30c3602c5e701770d4f1
Dougey-G <171282@student.asd20.org>
2019-02-13 15:10:26 -0700

    Worked on LINQ Problems.

---
+++ Semester 2/LINQ Problems/LINQ Problems/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //string result = Console.ReadLine();
+
+            // Console.WriteLine("Which Store just survived Bankruptcy, within the last month?");
+            // string[] stores = { "Sears" , "Toys R Us", "Radio Shack"};
+
+            //var query = stores.Select((stores, index) => new {index, str = stores.Substring(0, index) });
+
+            // if (result == "Sears")
+            // {
+            // Console.WriteLine("You are right!");
+            // }
+
+            // if (result == "toys r us")
+            // {
+            // Console.WriteLine("Sorry, try again");
+            // }
+
+            // if (result == "radio shack")
+            // {
+            // Console.WriteLine("Sorry, try again");
+            // }
+
+            string[] systems = { " Nintendo Switch", "Wii U", "Wii", "GameCube", "Nintendo 64", "Super NES"};
+
+            var query = systems.Select((system, index) => new { index, str = fruit.Substring(0, index) });
+
+            foreach (var obj in query)
+        }
+    }
+}
