7194680720a7aaccf75ccdc7d3b43b856d4b9f16
Dougey-G <171282@student.asd20.org>
2019-02-21 13:59:25 -0700

    Took a quiz on how to dupilcate the same word.

---
+++ Semester 2/stringTimes Quiz/stringTimes Quiz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringTimes_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.WriteLine(stringTimes("Hi", 2));
+            Console.WriteLine(stringTimes("Hi", 3));
+            Console.WriteLine(stringTimes("Hi", 1));
+            Console.ReadLine();
+        }
+
+        static public string stringTimes(string Word, int Times)
+        {
+            string times = "";
+
+            for (int i = 0; i < Times; i++)
+            {
+                times += Word;
+            }
+            return times;
+        }
+    }
+}
