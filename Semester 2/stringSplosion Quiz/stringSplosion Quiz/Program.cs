c13ee0d4c33ba79487ba36397321a6940f0896e6
Dougey-G <171282@student.asd20.org>
2019-03-11 13:03:51 -0600

    Took a quiz on how to print a Word, one Letter at a time.

---
+++ Semester 2/stringSplosion Quiz/stringSplosion Quiz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringSplosion_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string");
+            string result = Console.ReadLine();
+
+            for (int x = 0; x < result.Length; x++)
+            {
+                for (int y = 0; y < x + 1; y++)
+                {
+                    Console.Write(result[y]);
+                }
+            }
+            Console.ReadLine();        
+        }
+    }
+
+    
+}
