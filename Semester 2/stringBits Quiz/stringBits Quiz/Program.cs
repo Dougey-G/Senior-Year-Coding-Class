f75d46246793b089235c61aa4cee41390f193eef
Dougey-G <171282@student.asd20.org>
2019-03-19 15:36:44 -0600

    Took a quiz on how to print every other letter in a word.

---
+++ Semester 2/stringBits Quiz/stringBits Quiz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringBits_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string");
+            string result = Console.ReadLine();
+
+            for (int x = 0; x < result.Length; x++)
+            {
+                if(x % 2  == 0)
+                {
+                    Console.Write(result[x]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
