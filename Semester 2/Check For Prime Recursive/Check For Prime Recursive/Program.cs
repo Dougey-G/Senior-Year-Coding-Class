53178d660d9b26e2492697dd8b74d6c48f5e2e62
Dougey-G <171282@student.asd20.org>
2019-03-19 15:43:42 -0600

    Learned how to use Recursion to find prime and non prime numbers.

---
+++ Semester 2/Check For Prime Recursive/Check For Prime Recursive/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_For_Prime_Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what number do you want to check?");
+            int number = int.Parse(Console.ReadLine());
+            bool result = CheckForPrime(number);
+
+            if (result == true)
+            {
+                Console.WriteLine("It is prime.");
+            }
+
+            if (result == false)
+            {
+                Console.WriteLine("It is not prime.");
+            }
+            Console.ReadLine();
+        }
+        static bool CheckForPrime(int n, int divisor = 2)
+        {
+            if (n % divisor == 0)
+            { 
+                return false;
+            }
+
+            if (divisor == n - 1)
+            {
+                return true;
+            }
+            
+            return CheckForPrime(n, divisor + 1);
+        }
+        
+    }
+}
